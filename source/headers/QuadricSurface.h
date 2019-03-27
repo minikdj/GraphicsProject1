82545a51a4a42c4c03279c49c2121a12695b102b
minikdj <minikdj11@gmail.com>
2019-03-27 10:56:02 -0500

    added functions to quadric surface header

--- source/headers/QuadricSurface.h
+++ source/headers/QuadricSurface.h
@@ -52,3 +52,14 @@ public:
 
 };
 
+class Ellipsoid : public QuadricSurface
+{
+    public:
+    // a, b, and c for the equation of an ellipsoid
+    double a, b, c;
+
+    Ellipsoid(const dvec3 & position, const color & mat, double a, double b, double c);
+    Ellipsoid(const dvec3 & position, const Material & mat, double a, double b, double c);
+    friend HitRecord QuadricSurface::findClosestIntersection(const Ray & ray);
+
+};
